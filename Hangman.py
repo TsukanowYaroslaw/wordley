f1801c6e62bce4b71274358c51bad9eb7cfb4af4
TsukanowYaroslaw <tsukanow.ya@mail.ru>
2025-04-17 19:17:35 +0300

    End Game. Play agan?

--- Hangman.py
+++ Hangman.py
@@ -115,3 +115,40 @@ def  displayBoard(missedLetters, secretWord, correctLetters):
     for letter in blanks:
         print(letter, end=" ")
     print()
+
+#Ввод и исравнение
+def getGuess(alreadyGuessed):
+    while True:
+        print("Тема животные")
+        guess = input("Угадайте букву:")
+        guess = guess.lower()
+        if len(guess)!= 1:
+            print("Пожалуйста, введите ОДНУ РУССКУЮ БУКВУ")
+        elif guess not in 'абвгдеёжзийклмнопрстуфхцчшщьыъэюя':
+            print("Пожалуйста, введите БУКВУ")
+        elif guess in alreadyGuessed:
+            print("Буква введена. Напишите снова.")
+        else:
+            return guess
+        
+
+#Хотите сыграть ещё?
+while True:
+    displayBoard(missedLetters, secretWord, correctLetters)
+    guess = getGuess(missedLetters + correctLetters)   
+    if guess in secretWord:
+        correctLetters = correctLetters + guess
+        if len(correctLetters) == len(secretWord):
+            print("Вы выиграли")
+            displayBoard(missedLetters, secretWord, correctLetters)
+            answer, correctLetters, missedLetters, secretWord = play_again()
+            if answer != "да":
+                break
+    else:
+        missedLetters += guess
+        if len(missedLetters) == len(HANGMAN_PICS) - 1:
+            displayBoard(missedLetters, secretWord, correctLetters)
+            print("Вы проиграли")
+            answer, correctLetters, missedLetters, secretWord = play_again()
+            if answer != "да":
+                break
